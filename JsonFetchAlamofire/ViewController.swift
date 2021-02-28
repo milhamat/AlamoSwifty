77b34f02fd8d522a56e3af6967f05b7c2765a50b
milhamat <M477ashiddiq@gmail.com>
2021-02-28 19:29:55 +0700

    first final

--- JsonFetchAlamofire/ViewController.swift
+++ JsonFetchAlamofire/ViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var titleList = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        fetchData() 
     }
 
-
+    func fetchData(){
+        let urlAdress = "https://swapi.dev/api/films"
+        
+        AF.request(urlAdress, method: .get)
+            .responseJSON(completionHandler: { response in
+//            print(response)
+                let dataJSON = try? JSON(data: response.data!)
+//                print(dataJSON!["results"])
+                let myData = dataJSON!["results"]
+                
+                self.titleList.removeAll()
+                
+                for i in myData.arrayValue {
+//                    print(i)
+                    let title = i["title"].stringValue
+                    self.titleList.append(title)
+                    print(title)
+                }
+                self.tableView.reloadData()
+        })
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(titleList)
+        return titleList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = titleList[indexPath.row]
+        return cell!
+    }
 }
 
---
+++ Pods/Target Support Files/Pods-JsonFetchAlamofire/Pods-JsonFetchAlamofire-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JsonFetchAlamofire : NSObject
+@end
+@implementation PodsDummy_Pods_JsonFetchAlamofire
+@end
