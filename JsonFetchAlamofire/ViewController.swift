690908e3a47e4bf370ce5bab364b90f381c7619c
milhamat <M477ashiddiq@gmail.com>
2021-02-28 18:17:19 +0700

    Initial Commit

---
+++ JsonFetchAlamofire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JsonFetchAlamofire
+//
+//  Created by Muhammad Ilham Ashiddiq Tresnawan on 28/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
